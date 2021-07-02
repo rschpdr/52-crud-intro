c7125b99607d6dcf073f9551eeeb0a7f907237e1
rschpdr <pedro.resch@gmail.com>
2021-07-02 11:27:46 -0300

    added new about component

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+function About() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus odio
+      id ipsum maximus, sagittis consequat orci bibendum. Vivamus posuere eget
+      elit ac lacinia. Nullam nec mauris laoreet, scelerisque enim nec,
+      ullamcorper ipsum. Maecenas urna massa, dapibus in mauris et, volutpat
+      viverra risus. Vestibulum aliquam tellus id nisi volutpat ultrices. Etiam
+      scelerisque commodo malesuada. Nullam vehicula sem quam, nec facilisis
+      tellus fermentum ac. Ut dignissim odio at congue scelerisque. Phasellus
+      laoreet lacus non lectus eleifend scelerisque. Integer congue eget lectus
+      quis maximus. Nullam malesuada nisl at arcu blandit, placerat hendrerit
+      nunc elementum. Proin metus magna, tincidunt vel viverra ac, auctor sit
+      amet mauris. Ut at mollis odio. Donec eget interdum metus. Lorem ipsum
+      dolor sit amet, consectetur adipiscing elit. Maecenas id convallis ante.
+      Integer imperdiet libero at libero elementum faucibus. Vivamus sem dui,
+      imperdiet sit amet interdum mollis, congue suscipit neque. Nunc convallis
+      molestie ligula commodo placerat. Cras augue eros, hendrerit id lorem at,
+      finibus elementum lacus. Vestibulum nec metus quis diam vestibulum
+      dignissim id molestie massa. Mauris ut nisl purus. Suspendisse lobortis
+      nisi sed ultricies vestibulum. Curabitur malesuada dignissim lacinia.
+      Donec semper tempor ornare. Curabitur eleifend viverra rutrum. Donec
+      sodales mi turpis, eu pretium mi porttitor eu.
+    </p>
+  );
+}
+
+export default About;
